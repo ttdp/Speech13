deed910e94e8649d263399a387c644d021a99585
ttdp <tiantong.dev@gmail.com>
2020-12-31 08:16:28 +0800

    Initial Commit

---
+++ Speech13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Speech13
+//
+//  Created by Tian Tong on 12/31/20.
+//  Copyright © 2020 TTDP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
